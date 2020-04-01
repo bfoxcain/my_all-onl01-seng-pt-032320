6f894f0486b9bce8f1cdaec594a3d31e2dc234d1
Beverly Appleton <bfcain19@gmail.com>
2020-04-01 05:28:17 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,4 +1,4 @@
-require 'pry'
+
 
 def my_all?(collection)
   i = 0
@@ -8,4 +8,11 @@ def my_all?(collection)
     i += 1
   end
 
+  if block_return_values.include?(false)
+    false
+  else 
+    true
+  end
+
 end
+
